f9395aa6ff00e6d1079da47f11fff7d924a09d6a
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-04-17 00:56:13 +0600

    grid system fixs

--- src/components/Blogs/BlogComponents/BlogPosts.tsx
+++ src/components/Blogs/BlogComponents/BlogPosts.tsx
@@ -15,44 +15,42 @@ interface propsType {
 const BlogPosts = ({ post }: propsType) => {
   return (
     <>
-      <div className=" dark:bg-[#082f2b] rounded-md">
-        <div className=" grid grid-cols-12 gap-2 p-1">
-          <div className="h-full lg:col-span-3 col-span-12 overflow-hidden rounded-md flex justify-center items-center ">
-            <img
-              src={post.img}
-              alt=".."
-              loading="lazy"
-              className=" h-auto w-auto "
-            />
-          </div>
-          <div className="lg:col-span-9 col-span-12 flex flex-col justify-between gap-1 lg:pr-5 ">
-            <header>
-              <h1 className=" cursor-pointer text-xl underline underline-offset-4 ">
-                {post.blogHeader}
-              </h1>
-            </header>
-            <main>
-              <div className=" text-sm text-justify text-slate-300">
-                {post.blogBody.trim().length > 200
-                  ? post.blogBody.slice(0, 100) + "...."
-                  : post.blogBody}
-              </div>
-            </main>
-            <footer className=" flex justify-between items-center ">
-              <div className=" flex items-center gap-2 ">
-                <button className=" flex items-center gap-1 text-sm ">
-                  <FaThumbsUp fill="green" size={15} />
-                  <span>{post.likes}</span>
-                </button>
-                <button className=" flex items-center gap-1">
-                  <BiSolidCommentDetail fill="green" size={15} />
-                  <span>{post.comment.length}</span>
-                </button>
-              </div>
-              <div className=" text-slate-400 text-sm ">3M Read</div>
-              <div className=" text-slate-400 text-sm ">{post.date}</div>
-            </footer>
-          </div>
+      <div className=" bg-white dark:bg-[#082f2b] rounded-md">
+        <div className=" h-52 lg:h-36 overflow-hidden rounded-t-md flex justify-center items-center ">
+          <img
+            src={post.img}
+            alt=".."
+            loading="lazy"
+            className=" h-auto w-auto "
+          />
+        </div>
+        <div className="flex flex-col justify-between gap-1 p-3 ">
+          <header>
+            <h1 className=" cursor-pointer text-base font-semibold underline underline-offset-4 hover:text-orange-500 ">
+              {post.blogHeader}
+            </h1>
+          </header>
+          <main>
+            <div className=" text-sm text-justify text-slate-700 dark:text-slate-300">
+              {post.blogBody.trim().length > 200
+                ? post.blogBody.slice(0, 100) + "...."
+                : post.blogBody}
+            </div>
+          </main>
+          <footer className=" flex justify-between items-center ">
+            <div className=" flex items-center gap-2 ">
+              <button className=" flex items-center gap-1 text-sm ">
+                <FaThumbsUp fill="green" size={20} />
+                <span className=" text-sm ">{post.likes}</span>
+              </button>
+              <button className=" flex items-center gap-1">
+                <BiSolidCommentDetail fill="green" size={20} />
+                <span className=" text-sm ">{post.comment.length}</span>
+              </button>
+            </div>
+            <div className=" text-slate-400 text-sm ">3M Read</div>
+            <div className=" text-slate-400 text-sm ">{post.date}</div>
+          </footer>
         </div>
       </div>
     </>
