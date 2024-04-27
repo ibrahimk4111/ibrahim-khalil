f29b4d15fa8414154fe8aad1acbeac09779830cd
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-04-27 14:42:47 +0600

    blog padd fixs

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -3,7 +3,7 @@ import BlogPosts from "./BlogComponents/BlogPosts";
 
 const Blogs = () => {
   return (
-    <div className=" w-full p-2 dark:bg-gradient-to-l from-bgColorsInDark via-[#001f1b] to-bgColorsInDark ">
+    <div className=" snap-center md:ml-12 pt-16 px-2 pb-2 bg-slate-200 dark:bg-gradient-to-l from-bgColorsInDark via-[#001f1b] to-bgColorsInDark ">
       {/* left side or news feed */}
       <div className=" grid 2xl:grid-cols-6 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 ">
         {posts.map((post, index) => (
